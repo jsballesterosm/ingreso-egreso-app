4ec6f2a8d6d197d4c3e68d359a2b5f5d4f97b1a1
Juan Sebastian Ballesteros <jballesteros@octansit.com>
2021-05-31 09:20:33 -0500

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCnO38XqLU6B6-Y7nRYZhKU70szFGBphv4',
-    authDomain: 'ingresos-egresos-162f6.firebaseapp.com',
-    projectId: 'ingresos-egresos-162f6',
-    storageBucket: 'ingresos-egresos-162f6.appspot.com',
-    messagingSenderId: '486253583982',
-    appId: '1:486253583982:web:207ae2de13eb82fc04907f'
+
   }
 };
 
